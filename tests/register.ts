ed119cb08435302a5b0e482287128870af34a17b
Juliana Velasquez <juliana.velasquez.estrada@gmail.com>
2025-06-14 15:14:20 -0500

    Ranaming test cases to English and starting to implement POM

--- tests/register.ts
+++ tests/register.ts
@@ -1,40 +1,44 @@
 import { test, expect } from '@playwright/test';
+import { RegisterPage } from '../pages/registerPage';
 
-test('TC-1 Verificacion de elementos visuales en la pagina de registro', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await expect(page.locator('input[name=firstName]')).toBeVisible();
-  await expect(page.locator('input[name=lastName]')).toBeVisible();
-  await expect(page.locator('input[name=email]')).toBeVisible();
-  await expect(page.locator('input[name=password]')).toBeVisible();
-  await expect(page.getByTestId('boton-registrarse')).toBeVisible();
-  //await page.waitForTimeout(5000);
+let registerPage: RegisterPage;
+
+test.beforeEach(async({page}) => {
+  registerPage = new RegisterPage(page);
+  await registerPage.visitRegistrationPage();
+});
+
+test('TC-1 Verify visual elements on the registration page', async ({ page }) => {
+  await expect(registerPage.firstNameInput).toBeVisible();
+  await expect(registerPage.lastNameInput).toBeVisible();
+  await expect(registerPage.emailInput).toBeVisible();
+  await expect(registerPage.passwordInput).toBeVisible();
+  await expect(registerPage.registerButton).toBeVisible();
 
 });
 
-test('TC-2 Verificar que el boton de registro esta deshabilitado por defecto', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await expect(page.getByTestId('boton-registrarse')).toBeDisabled();
+test('TC-2 Verify that the registration button is disabled by default.', async ({ page }) => {
+  await registerPage.visitRegistrationPage();
+  await expect(registerPage.registerButton).toBeDisabled();
 
 });
 
-test('TC-3 Verificar que el boton de registro se habilite luego de diligenciar los campos obligatorios', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.locator('input[name=firstName]').fill('Juliana');
-  await page.locator('input[name=lastName]').fill('Velasquez');
-  await page.locator('input[name=email]').fill('julia@velasquez.com');
-  await page.locator('input[name=password]').fill('password123');
-  await expect(page.getByTestId('boton-registrarse')).toBeEnabled();
+test('TC-3 Verify that the registration button is enabled after completing the required fields.', async ({ page }) => {
+ const registerPage = new RegisterPage(page);
+  await registerPage.visitRegistrationPage();
+  await registerPage.completeRegistrationForm('Juliana','Velasquez','julia@velasquez.com','password123');
+  await registerPage.clickOnRegistrationButton();
 
 });
 
-test('TC-4 Verificar redireccionamiento a pagina de inicio de sesion al hacer clic en el botón de registro', async ({ page }) => {
+test('TC-4 Verify the redirection to the login page when clicking the registration button', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await page.getByTestId('boton-login-header-signup').click();
   await expect(page).toHaveURL('http://localhost:3000/login');
 
 });
 
-test('TC-5 Verificar registro exitoso con datos validos', async ({ page }) => {
+test('TC-5 Verify successful registration with valid data', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await page.locator('input[name=firstName]').fill('Juliana');
   await page.locator('input[name=lastName]').fill('Velasquez');
@@ -44,7 +48,7 @@ test('TC-5 Verificar registro exitoso con datos validos', async ({ page }) => {
   await expect(page.getByText('Registro exitoso')).toBeVisible();
 });
 
-test('TC-6 Verificar que un usuario no pueda registarse con un correo electronico ya existente', async ({ page }) => {
+test('TC-6 Verify that a user cannot register with an existing email address', async ({ page }) => {
   const email = 'juliana'+Date.now().toString()+'@gmail.com';
   await page.goto('http://localhost:3000/');
   await page.locator('input[name=firstName]').fill('Juliana');
